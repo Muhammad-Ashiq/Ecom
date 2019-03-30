57cec31a28611924358e90ae171016da4aa225fd
Muhammad Ashiq <Muhammad-ashiq.github.com>
2019-03-30 16:31:48 +0500

    First Migration

---
+++ Ecom/Ecom.Database/EContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Ecom.Models;
+
+namespace Ecom.Database
+{
+    public class EContext:DbContext
+    {
+        public EContext() : base("DefaultConnection")
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
